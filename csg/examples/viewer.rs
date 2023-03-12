b5dac2582316358c658f9650dba071af9280cf81
Michael Phillips <mrclean71774@gmail.com>
2023-03-11 23:21:17 -0500

    Add example using the Viewer struct

--- csg/examples/viewer.rs
+++ csg/examples/viewer.rs
@@ -1,1 +1,36 @@
-main(){}
+// MIT License
+//
+// Copyright (c) 2023 Michael H. Phillips
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//! In this example we load the Suzanne mesh and look at it's vertices and edges
+//! with the viewer.
+
+use csg::{Mesh, Viewer};
+
+fn main() {
+  let suzanne = Mesh::load_stl("in/suzanne.stl");
+  let mut viewer = Viewer::new(0.1, 0.05, 12);
+
+  viewer.add_verts(suzanne.vertices());
+  viewer.add_edges(suzanne.edges());
+
+  viewer.render().save_stl_bin("out/SuzanneViewer.stl");
+}
